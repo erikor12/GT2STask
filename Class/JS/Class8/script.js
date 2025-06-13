ed2b08d84fdd92b593e51c59d3608c46f2b12c1d
erikor12 <erikburs78@gmail.com>
2025-06-13 15:12:06 -0300

    dsds

---
+++ Class/JS/Class8/script.js
@@ -0,0 +1,14 @@
+let engy = 80;
+if (engy >= 80) {
+    console.log("100% ");
+} else if (engy >= 60) {
+    console.log("80%");
+} else if (engy >= 40) {
+    console.log("60%");
+} else if (engy >= 20) {
+    console.log("40%");
+} else {
+    console.log("20%");
+}
+
+
