b5e70bfbae4a1c85d869ff18bc7962ddb3e1abcb
erikor12 <erikburs78@gmail.com>
2025-07-04 16:38:40 -0300

    ji

--- Juego de Carreras/script.js
+++ Juego de Carreras/script.js
@@ -1,26 +1,42 @@
 const blu_car = document.querySelector("#blue_car");
 const red_car = document.querySelector("#red_car");
+const track = document.querySelector("#track");
 
 let blue_car_x = 0;
 let red_car_x = 0;
 
-const track = document.querySelector("#track");
+const jump = 20;
+const finish = track.offsetWidth - blu_car.offsetWidth - 20;
 
-document.addEventListener("keyup", (event) => {
-    if (event.key === "ArrowRight") {
-        blue_car_x += 10;
-        blu_car.style.left = `${blue_car_x}px`;
+window.addEventListener ("keyup", (e) => {
+    console.log(e);
+    if (e.key === 'd') {
+        red_car_x += jump;
+        if (red_car_x > finish) {
+            red_car_x = finish
+        }
+        red_car.style.marginLeft = red_car_x + "px";
+        if (red_car_x >= finish) {
+            this.alert("Red Car Wins");
+            reset();
+        }
     }
-    if (event.key === "ArrowLeft") {
-        blue_car_x -= 10;
-        blu_car.style.left = `${blue_car_x}px`;
+    if (e.key === 'a') {
+        blue_car_x += jump;
+        if (blue_car_x > finish) {
+            blue_car_x = finish
+        }
+        blu_car.style.marginLeft = blue_car_x + "px";
+        if (blue_car_x >= finish) {
+            this.alert("Blue Car Wins");
+            reset();
+        }
     }
-    if (event.key === "ArrowUp") {
-        red_car_x -= 10;
-        red_car.style.left = `${red_car_x}px`;
-    }
-    if (event.key === "ArrowDown") {
-        red_car_x += 10;
-        red_car.style.left = `${red_car_x}px`;
-    }
-});
+})
+
+function reset() {
+    blue_car_x = 0;
+    red_car_x = 0;
+    red_car.style.marginLeft = "0px";
+    blu_car.style.marginLeft = "0px";
+}
